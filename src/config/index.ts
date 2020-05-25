4bc2a30a3b15d2816d48e71b25c5bd8303cd24f9
Jean-Baptiste Terrazzoni <jterrazz@icloud.com>
2020-05-26 00:45:35 +0200

    logs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,28 @@
-const config = {
-  SERVER: {
-    PORT: 3000,
-  },
-};
+import * as Joi from '@hapi/joi';
+import { TomConsole, ConsoleLevel } from '@tom';
 
-export default config;
+const tomConsole = new TomConsole('CONFIG');
+
+const envSchema = Joi.object({
+    ENV: Joi.string().default('development'),
+    LOGS: Joi.number().default(0),
+
+    // SERVER
+    SERVER_PORT: Joi.number().default(3000),
+
+    // DATABASE
+    DATABASE_HOST: Joi.string().default('127.0.0.1'),
+})
+    .unknown()
+    .required();
+
+const { error, value: envValues } = envSchema.validate(process.env);
+
+if (error) {
+    tomConsole.print(`Environment variable error: ${error.message}`, ConsoleLevel.ERROR);
+    process.exit(1);
+}
+
+tomConsole.levelThreshold = envValues.LOGS;
+
+export default envValues;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import app from './app';
 import config from '@config';
+import { TomConsole, ConsoleLevel } from '@tom';
 
-app.listen(config.SERVER.PORT, () => {
-    console.info(`Listening to ${config.SERVER.PORT} 🚀`);
+const tomConsole = new TomConsole('CONFIG');
+
+app.listen(config.SERVER_PORT, () => {
+    tomConsole.print(`Server is running on port ${config.SERVER_PORT}`, ConsoleLevel.INFO);
 });
