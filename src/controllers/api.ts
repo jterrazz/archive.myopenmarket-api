e7e2e3bacf5dda7213873603ea7035d90c9c52be
Jean-Baptiste Terrazzoni <jterrazz@icloud.com>
2020-06-13 22:27:01 +0200

    update li

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -3,8 +3,6 @@ import { Middleware } from 'koa';
 import config from '@config';
 
 export const getStateController: Middleware = async (ctx, next) => {
-    ctx.tracker.track(ctx.trackerEvents.GET_STATE);
-
     ctx.body = {
         version: config.API_VERSION,
         state: 'OK',
